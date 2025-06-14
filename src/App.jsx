abf3fd61f8d319d4147b6d18574b8c001c10db06
TheOnly1TY <anifowosetemitayo5@gmail.com>
2025-06-14 03:43:53 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  font-size: 2rem;
+  font-weight: 800;
+`;
+
+export default function App() {
+  return <StyledApp>DESIGNO WEBSITE</StyledApp>;
+}
